c4db6700de83da93e86c09e4f6f0fd6e67fac100
Douglas Gardner <douglas@chippy.ch>
2017-08-02 16:37:06 +0100

    Db fix

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/scrutineer')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/scrutineer', {server: {auto_reconnect: true}})
 
 var db = module.exports = mongoose.connection
 
 db.on('error', function (err) {
-  console.error(err)
+  console.error('DB PROBLEM', err)
 })
 db.once('open', function () {
   console.log('Connected to database.')
