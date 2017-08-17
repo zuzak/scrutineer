96e844410c1e95ebf8b1b820b471b829ead0f960
Douglas Gardner <douglas@chippy.ch>
2017-08-17 17:48:56 +0100

    Add observation count to homepage

--- routes/home.js
+++ routes/home.js
@@ -1,14 +1,22 @@
 var app = require('../app')
 var Station = require('../models/station.js')
+var Observation = require('../models/observation')
+
 app.get('/', function (req, res, next) {
-  Station.count({}, function (err, stationCount) {
+  Observation.count({}, function (err, observationCount) {
     if (err) next(err)
-    res.render('index.pug', {
-      title: 'Scrutineer',
-      data: [{
-        caption: 'polling stations in database',
-        value: stationCount
-      }]
+    Station.count({}, function (err2, stationCount) {
+      if (err2) next(err)
+      res.render('index.pug', {
+        title: 'Scrutineer',
+        data: [{
+          caption: 'polling stations in database',
+          value: stationCount
+        }, {
+          caption: 'observation forms started',
+          value: observationCount
+        }]
+      })
     })
   })
 })
