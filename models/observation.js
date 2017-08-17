ecfa1cdfc4b7d5ce314c702a0288e7c7de58bba6
Douglas Gardner <douglas@chippy.ch>
2017-08-17 13:25:41 +0100

    Begin saving observation

--- models/observation.js
+++ models/observation.js
@@ -1,61 +1,57 @@
 var mongoose = require('mongoose')
 
 var schema = new mongoose.Schema({
-  territory: String,
-  lang: String,
-  address: String,
-  postcode: String,
-  fax: String,
-  tel: String,
-  email: String
+  station_id: String,
+  council_id: String,
+  user_id: String,
+
+  // station approach
+  'freeFromUndueInfluence': Boolean,
+  'hadDefacedOrDamagedNotices': Boolean,
+  'wasEasyToFind': Boolean,
+  'wasAccessibleToWheelchairs': Boolean,
+  'wasFreeFromHazards': Boolean,
+  'hadQueues': Boolean,
+  'hadOrderlyQueues': Boolean,
+  'hadParking': Boolean,
+  'hadDisabledParking': Boolean,
+  'hadTellers': Boolean,
+  'hadTellersFrom': Array,
+
+  // station entry
+  'ingress': String, // Time
+  'egress': String, // Time
+
+  'card-check': Array,
+
+  // polling procedure
+  'sawElectors': Boolean,
+  'name-and-address': String,
+  'crossing-register': String,
+  'distributing-ballot': String,
+  'prefolding-ballot': String,
+  'uim': String,
+  'secrecy': String,
+
+  // polling place
+  'hadSeatsAvailable': Boolean,
+  hadPrivateBooths: Boolean,
+  hadPencils: Boolean,
+  hadLowLevelBooth: Boolean,
+  hadCctv: Boolean,
+  hadCctvThatUndermines: Boolean,
+  wasWellLit: Boolean,
+  hadNotices: Boolean,
+  hadGuidanceToVoters: Boolean,
+  hadInstructionsToVoters: Boolean,
+  hadSealedBoxes: Boolean,
+  hadPostalBallotsHandedIn: Boolean,
+  wereWearingNeutralColours: Boolean,
+  hadNonElectors: Boolean,
+  hadNonElectorsType: Array,
+
+  // verdicts
+  upheldSuffrage: Boolean,
+  upheldIntegrity: Boolean,
+  comments: String
 })
-
-var Commission = module.exports = mongoose.model('Commission', schema)
-
-var commissions = [
-  {
-    territory: 'E',
-    lang: 'en',
-    address: 'The Electoral Commission\n3 Bunhill Row\nLondon',
-    postcode: 'EC1Y 8YZ',
-    tel: '020 7271 0500',
-    fax: '020 7271 0505',
-    email: 'info@electoralcommission.org.uk'
-  },
-  {
-    territory: 'S',
-    address: 'The Electoral Commission\nLothian Chambers\n59-63 George IV Bridge\nEdinburgh',
-    lang: 'en',
-    postcode: 'EH1 1RN',
-    tel: '0333 103 1928',
-    fax: '0131 225 0205',
-    email: 'infoscotland@electoralcommission.org.uk'
-  },
-  {
-    territory: 'W',
-    address: 'The Electoral Commission\nCompanies House\nCrown Way\nCardiff',
-    lang: 'en',
-    postcode: 'CF14 3UZ',
-    tel: '0333 103 1929',
-    fax: '029 2038 0996',
-    email: 'infowales@electoralcommission.org.uk'
-  },
-  {
-    territory: 'W',
-    address: 'Y Comisiwn Etholiadol\nTŷ’r Cwmnïau\nFfordd y Goron\nCaerdydd',
-    lang: 'cy',
-    postcode: 'CF14 3UZ',
-    tel: '0333 103 1929',
-    fax: '029 2038 0996',
-    email: 'gwybodaeth@comisiwnetholiadol.org.uk'
-  }
-]
-
-for (var i = 0; i < commissions.length; i++) {
-  Commission.update({
-    territory: commissions[i].territory,
-    lang: commissions[i].lang
-  }, commissions[i], {upsert: true}, function (err) {
-    if (err) throw err
-  })
-}
